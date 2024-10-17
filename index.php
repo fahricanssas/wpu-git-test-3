db53311a86d14637f0b0562430016aeb0bd5bb24
fahricanssas <hidayatfahri111@gmail.com>
2024-10-17 12:27:26 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>hello world!</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque eum sit animi doloremque quod error voluptatum ut, aliquam vitae officiis id inventore recusandae, porro eos vero dignissimos excepturi molestiae soluta?</p>
+    <ul>
+        <li>satu</li>
+        <li> dua  </li>
+        
+    </ul>
 </body>
 </html>
